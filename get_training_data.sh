3f8bdafaf1163ab40c50e058d4e47f2eb8783b0d
Oscar Ablinger <dev.ablinger@gmail.com>
2019-11-30 16:41:41 +0100

    Never download pictures parallel

--- get_training_data.sh
+++ get_training_data.sh
@@ -11,11 +11,8 @@ for i in $(seq 1 5); do
     current_dir="$output_dir/0${i}_${dir_names[$((i-1))]}"
     echo "download files into $current_dir"
 
-    curl --silent --parallel --create-dirs "$base_url/$i/[1-1200].jpg" -o "$current_dir/#1.jpg" 2> /dev/null
-    if [ $? -ne 0 ]; then
-      # try again if curl fails on windows (--parallel option not working)
-      curl --silent --create-dirs "$base_url/$i/[1-1200].jpg" -o "$current_dir/#1.jpg" 2> /dev/null
-    fi
+    # Do not use --parallel as that results in a lot of corrupted files
+    curl --silent --create-dirs "$base_url/$i/[1-1200].jpg" -o "$current_dir/#1.jpg" 2> /dev/null
 done
 
 echo "download finished"
