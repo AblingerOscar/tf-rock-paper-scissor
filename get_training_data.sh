e5132f5bc69820b6f70c615de0b0cf1c7868855b
Florian Mysliwietz <myslflo1996@yahoo.de>
2019-11-30 14:35:18 +0100

    Resolve requested changes for get_training_data script

--- get_training_data.sh
+++ get_training_data.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-BASE_URL="https://raw.githubusercontent.com/akshaybahadur21/Emojinator/master/Rock_Paper_Scissor_Lizard_Spock/RPS_data"
-OUTPUT_DIR=${PARENT_PATH}"/jupyter/volume/dataset"
+base_url="https://raw.githubusercontent.com/akshaybahadur21/Emojinator/master/Rock_Paper_Scissor_Lizard_Spock/RPS_data"
+output_dir="$parent_path/jupyter/volume/dataset"
 
-DIR_NAMES=('rock' 'paper' 'scissors' 'lizard' 'spock')
+dir_names=('rock' 'paper' 'scissors' 'lizard' 'spock')
 
 for i in $(seq 1 5); do
-    CURRENT_DIR=${OUTPUT_DIR}"/0"${i}"_"${DIR_NAMES[$((i-1))]}
-    echo "download files into "${CURRENT_DIR}
-    curl --silent --parallel --create-dirs ${BASE_URL}"/"${i}"/[1-1200].jpg" -o ${CURRENT_DIR}"/#1.jpg"
+    current_dir="$output_dir/0${i}_${dir_names[$((i-1))]}"
+    echo "download files into $current_dir"
+
+    curl --silent --parallel --create-dirs "$base_url/$i/[1-1200].jpg" -o "$current_dir/#1.jpg" 2> /dev/null
+    if [ $? -ne 0 ]; then
+      # try again if curl fails on windows (--parallel option not working)
+      curl --silent --create-dirs "$base_url/$i/[1-1200].jpg" -o "$current_dir/#1.jpg" 2> /dev/null
+    fi
 done
+
 echo "download finished"
